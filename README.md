fca655a6acd8db5c1e39f7fb7a65668c5219c8d0
error-BSOD64 <ricardoassis2003@gmail.com>
2022-04-02 19:15:21 +0100

    Added commands to install git and python3

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 Here you'll find automated scripts that eased my command-line tasks.
 Feel free to use it.
-Plus, it's open-source!
 
-!!! You will need Python and git to run and download the scripts. !!!
+!!! You will need Python 3 and git to run and download the scripts. !!!
 
-No scripts available at the moment...
+Commands
+
+Arch-Based: "sudo pacman -Sy && sudo pacman -S git python3"
+
+Debian-Based: "sudo apt update && sudo apt install git python3"
+
--- install-software.py
+++ install-software.py
@@ -3,7 +3,7 @@ from time import sleep
 
 def installDebian():
     system("cd ~/")
-    system("sudo apt update && sudo apt install wget git make curl snapd flatpak")
+    system("sudo apt update && sudo apt install wget gcc make curl snapd flatpak")
     system("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
     system("wget https://flathub.org/repo/appstream/org.libretro.RetroArch.flatpakref")
     system("wget https://github.com/gbdk-2020/gbdk-2020/releases/download/4.0.6/gbdk-linux64.tar.gz")
@@ -15,7 +15,7 @@ def installDebian():
     system("code --install-extension ms-vscode.notepadplusplus-keybindings")
     system("code --install-extension peaceshi.syntax-highlight")
     system("code --install-extension emulicious.emulicious-debugger")
-    
+    system("code --install-extension donaldhays.rgbds-z80")
     system("sudo snap install vlc")
 
 def main():
