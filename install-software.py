0e14a73c38eb883234f93e94a1447cebf6b521bc
error-BSOD64 <ricardoassis2003@gmail.com>
2022-04-02 21:04:04 +0100

    Completed installDebian Funcion

--- install-software.py
+++ install-software.py
@@ -1,11 +1,23 @@
 from os import system
 from time import sleep
 
+def installFedora():
+    print("Coming Soon...")
+
+def installArch():
+    print("Coming Soon...")
+
 def installDebian():
     system("cd ~/")
-    system("sudo apt update && sudo apt install wget gcc make curl snapd flatpak")
+    system("mkdir files")
+    system("cd ~/files")
+    system("sudo apt update && sudo apt install wget gcc make curl snapd flatpak rpi-imager gparted")
     system("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
     system("wget https://flathub.org/repo/appstream/org.libretro.RetroArch.flatpakref")
+    system("wget https://dl.flathub.org/repo/appstream/org.audacityteam.Audacity.flatpakref")
+    system("wget https://dl.flathub.org/repo/appstream/org.shotcut.Shotcut.flatpakref")
+    system("wget https://dl.flathub.org/repo/appstream/com.obsproject.Studio.flatpakref")
+    system("wget https://dl.flathub.org/repo/appstream/com.discordapp.Discord.flatpakref")
     system("wget https://github.com/gbdk-2020/gbdk-2020/releases/download/4.0.6/gbdk-linux64.tar.gz")
     system("sudo tar -xzvf gbdk-linux64.tar.gz")
     system("sudo mv gbdk /")
@@ -31,6 +43,10 @@ def main():
         system("clear")
         installDebian()
         print("End of multi-stage job.")
+    if decision == str(2):
+        installArch()
+    if decision == str(3):
+        installFedora()
     print("Program will exit...")
 
 main()
