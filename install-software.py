0d02a58700af5e52cb257c3d2532174d64ce277f
error-BSOD64 <ricardoassis2003@gmail.com>
2022-04-02 18:54:09 +0100

    Added a few commands to installDebian Function

--- install-software.py
+++ install-software.py
@@ -1,15 +1,22 @@
 from os import system
 from time import sleep
 
-
 def installDebian():
     system("cd ~/")
     system("sudo apt update && sudo apt install wget git make curl snapd flatpak")
     system("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
     system("wget https://flathub.org/repo/appstream/org.libretro.RetroArch.flatpakref")
     system("wget https://github.com/gbdk-2020/gbdk-2020/releases/download/4.0.6/gbdk-linux64.tar.gz")
-    system("sudo tar -xzvf gbdk-linux64.tar.gz ./")
+    system("sudo tar -xzvf gbdk-linux64.tar.gz")
     system("sudo mv gbdk /")
+    system("sudo snap install --classic code")
+    system("code --install-extension ms-python.python")
+    system("code --install-extension ms-vscode.cpptools")
+    system("code --install-extension ms-vscode.notepadplusplus-keybindings")
+    system("code --install-extension peaceshi.syntax-highlight")
+    system("code --install-extension emulicious.emulicious-debugger")
+    
+    system("sudo snap install vlc")
 
 def main():
     print("Select the distro you are using:")
